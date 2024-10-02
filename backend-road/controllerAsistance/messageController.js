2b6ad691cda4abb20988dc362bc7dcc17f7eaf2b
syedbadurudeeny <baduru2010@gmail.com>
2024-10-02 14:58:21 +0530

    first commit

--- backend-road/controllerAsistance/messageController.js
+++ backend-road/controllerAsistance/messageController.js
@@ -1,28 +1,28 @@
-const asyncHandler = require('express-async-handler');
-const twilio = require('twilio');
+// const asyncHandler = require('express-async-handler');
+// const twilio = require('twilio');
 
-const  sendSms = (req,res) => {
+// const  sendSms = (req,res) => {
 
-    const {content,to} = req.body;
+//     const {content,to} = req.body;
 
-    if(!content || !to) {
-        res.status(400);
-        throw new Error("All Fields Are Not Empty");
-    }
+//     if(!content || !to) {
+//         res.status(400);
+//         throw new Error("All Fields Are Not Empty");
+//     }
 
-    const client = new twilio(process.env.ACCOUNT_SID , process.env.AUTH_TOKEN)
-    console.log(client)
-    return client.messages
-    .create({body:content, from: '+14159928847' , to: `+91${to}` })
-    .then(message => {
-        console.log(message,"message sent");
-    })
-    .catch(err =>{
-        console.log(err,"message not sent");
-    })
+//     const client = new twilio(process.env.ACCOUNT_SID , process.env.AUTH_TOKEN)
+//     console.log(client)
+//     return client.messages
+//     .create({body:content, from: '+14159928847' , to: `+91${to}` })
+//     .then(message => {
+//         console.log(message,"message sent");
+//     })
+//     .catch(err =>{
+//         console.log(err,"message not sent");
+//     })
     
-}
+// }
 
 
 
-module.exports = {sendSms}
+// module.exports = {sendSms}
